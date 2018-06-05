c563468d74f134214b66e70acc41f02228c8494c
Rick de Jong <rick.de.jong2@hva.nl>
2018-06-05 13:25:39 +0200

    Erik is een held

---
+++ SeriesGuide/src/test/java/com/battlelancer/seriesguide/test/FuckTest.java
@@ -0,0 +1,11 @@
+package com.battlelancer.seriesguide.test;
+
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class FuckTest {
+
+}
