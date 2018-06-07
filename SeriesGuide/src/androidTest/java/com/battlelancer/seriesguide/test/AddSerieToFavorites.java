56de379f67d1393b8dd3baf1e72fe678cfd85b24
Rick de Jong <rick.de.jong2@hva.nl>
2018-06-07 12:02:43 +0200

    Transfer to Mac

--- SeriesGuide/src/androidTest/java/com/battlelancer/seriesguide/test/AddSerieToFavorites.java
+++ SeriesGuide/src/androidTest/java/com/battlelancer/seriesguide/test/AddSerieToFavorites.java
@@ -1,11 +1,13 @@
 package com.battlelancer.seriesguide.test;
 
+import android.provider.Settings;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 
 import com.battlelancer.seriesguide.R;
 import com.battlelancer.seriesguide.ui.ShowsActivity;
+import com.linkedin.android.testbutler.TestButler;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -16,9 +18,10 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.junit.matchers.JUnitMatchers.containsString;
+import static org.hamcrest.core.AllOf.allOf;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -30,11 +33,23 @@ public class AddSerieToFavorites {
     @Test
     public void addSerieToFavoritesList() {
         String serieName = "Prison Break";
+        String littleBitOfSynopsis = "Due to a political";
 
         onView(withId(R.id.buttonShowsAdd)).perform(click());
         onView(withId(R.id.editTextSearchBar)).perform(typeText(serieName), pressImeActionButton());
-        onView(withId(R.id.seriesname)).check(matches(withText(containsString(serieName))));
-        onView(withId(R.id.seriesname)).perform(click());
+        onView(allOf(withId(R.id.textViewAddTitle), withText(serieName))).perform(click());
+        onView(withId(R.id.buttonPositive)).perform(click());
+    }
+
+    @Test
+    public void systemCanNotLoadSerieList() {
+        String serieName = "Game of Thrones";
+
+        TestButler.setWifiState(false);
+
+        onView(withId(R.id.buttonShowsAdd)).perform(click());
+        onView(withId(R.id.editTextSearchBar)).perform(typeText(serieName), pressImeActionButton());
+        onView(withId(R.id.emptyViewAdd)).check(matches(isDisplayed()));
     }
 
 }
